3764b7df2595377e2ca7ec00993641d8e2f5dc4d
Kartheek <kartheek.sprt@gmail.com>
2018-10-19 17:06:11 +0530

    Added code for saving the custom objects in Plist file.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -13,14 +13,18 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]();
     
-    let userDefaults = UserDefaults.standard;
+//    let userDefaults = UserDefaults.standard;
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist");
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let itemsList = userDefaults.array(forKey: "TodoListItem") as? [Item]{
-            itemArray = itemsList;
-        }
+        print("plist file path : \(String(describing: dataFilePath))")
+//        if let itemsList = userDefaults.array(forKey: "TodoListItem") as? [Item]{
+//            itemArray = itemsList;
+//        }
+        loadData()
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -32,8 +36,11 @@ class TodoListViewController: UITableViewController {
                 let item = Item();
                 item.title = todoTitle;
                 self.itemArray.append(item);
+            
+                self.saveDetails()
+            
                 //Updating the data in the userDefaults
-                self.userDefaults.setValue(self.itemArray, forKey: "TodoListItem");
+//                self.userDefaults.setValue(self.itemArray, forKey: "TodoListItem");
                 self.listTableView.reloadData();
             }
         }
@@ -47,7 +54,25 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    private func configureTableView(){
+    private func saveDetails(){
+        let encoder = PropertyListEncoder();
+        do{
+            let data = try encoder.encode(self.itemArray);
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("Error encoding the item array\(error)")
+        }
+    }
+    
+    private func loadData(){
+        if  let data = try? Data(contentsOf: dataFilePath!){
+            do{
+                let decoder = PropertyListDecoder();
+                itemArray = try decoder.decode([Item].self, from: data);
+            }catch{
+                print("Error in decoding the items : ");
+            }
+      }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -75,14 +100,13 @@ class TodoListViewController: UITableViewController {
         print("Clicked row \(indexPath.row), item : \(itemArray[indexPath.row])")
         
         let tableCell =  tableView.cellForRow(at: indexPath);
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done;
         if itemArray[indexPath.row].done == false {
-            itemArray[indexPath.row].done = true;
-             tableCell?.accessoryType = .checkmark;
-        }else{
-            itemArray[indexPath.row].done = false;
              tableCell?.accessoryType = .none;
+        }else{
+             tableCell?.accessoryType = .checkmark;
         }
+        saveDetails();
 //        if(tableCell?.accessoryType == UITableViewCellAccessoryType.checkmark){
 //
 //        }else{
